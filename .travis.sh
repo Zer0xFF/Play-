fab418e488dc0d9daf852c806b831ffece8ef36b
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-10 02:21:29 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -3,10 +3,12 @@
 travis_before_install() 
 {
     if [ "$TARGET_OS" = "Linux" ]; then
+        wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
         sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+        sudo apt-add-repository --yes 'deb https://apt.kitware.com/ubuntu/ xenial main'
         if [ "$TARGET_ARCH" = "ARM64" ]; then
             sudo apt update -qq
-            sudo apt install -y gcc-9 g++-9 qtbase5-dev libqt5x11extras5-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev libalut-dev libevdev-dev libgles2-mesa-dev
+            sudo apt install -y gcc-9 g++-9 qtbase5-dev libqt5x11extras5-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev libalut-dev libevdev-dev libgles2-mesa-dev cmake
         else
             wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
             chmod a+x linuxdeployqt*.AppImage
