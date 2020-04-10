6d1682b847bd7e14fdb2a33fbf6be3c8d3d10ade
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-10 02:25:54 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,7 @@ travis_before_install()
         sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
         sudo apt-add-repository --yes 'deb https://apt.kitware.com/ubuntu/ xenial main'
         if [ "$TARGET_ARCH" = "ARM64" ]; then
-            sudo apt update -qq
+            sudo apt update
             sudo apt install -y gcc-9 g++-9 qtbase5-dev libqt5x11extras5-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev libalut-dev libevdev-dev libgles2-mesa-dev cmake
         else
             wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
