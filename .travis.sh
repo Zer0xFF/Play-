1b485cb594d64700fe51a572e36ec9b7a317b254
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-10 02:03:09 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,8 @@ travis_before_install()
         if [ "$TARGET_ARCH" = "ARM64" ]; then
             sudo apt remove --auto-remove gcc g++
             sudo apt update -qq
-            dpkg --get-selections | grep hold
+            ping ppa.launchpad.net
+            dpkg --get-selections
             sudo aptitude install -y gcc-9 g++-9 qtbase5-dev libqt5x11extras5-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev libalut-dev libevdev-dev libgles2-mesa-dev
         else
             wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
