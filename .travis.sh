5edff1107fe1bceb5e5f97c75b9c36285a0c0d4d
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-10 04:39:58 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -11,7 +11,7 @@ travis_before_install()
             sudo apt install -y gcc-9 g++-9 qtbase5-dev libqt5x11extras5-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev libalut-dev libevdev-dev libgles2-mesa-dev
             wget https://madnation.net/uploads/cmake/1bb78-installer.sh
             chmod 755 1bb78-installer.sh
-            sudo sh 1bb78-installer.sh --skip-license -prefix=/usr
+            sudo sh 1bb78-installer.sh --skip-license -prefix=/usr/local
         else
             wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
             chmod a+x linuxdeployqt*.AppImage
