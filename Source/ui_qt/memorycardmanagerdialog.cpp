0ccf329c0da8b6fa9c43c2d95295eb1801bcb0e2
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-12-31 11:43:08 +0000

    Qt: replace removed function (change compatible with qt5)

--- Source/ui_qt/memorycardmanagerdialog.cpp
+++ Source/ui_qt/memorycardmanagerdialog.cpp
@@ -100,7 +100,7 @@ void MemoryCardManagerDialog::on_savelistWidget_currentRowChanged(int currentRow
 	{
 		const CSave* save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
 		QDateTime* dt = new QDateTime;
-		dt->setTime_t(save->GetLastModificationTime());
+		dt->setSecsSinceEpoch(save->GetLastModificationTime());
 		QString datetime = dt->toUTC().toString("hh:mm - dd.MM.yyyy");
 
 		ui->label_name->setText(QString::fromWCharArray(save->GetName()));
